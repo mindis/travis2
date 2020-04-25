0f67b269e534b048c7c2003157b69f763a982084
matiasnferrero <matiasnferrero@gmail.com>
2020-04-25 18:07:29 +0200

    first commi

---
+++ setup.py
@@ -0,0 +1,8 @@
+
+
+
+finished_successfully = False
+print("fin")
+#a + 2 ### should fail 
+finished_successfully = True
+print("finished_successfully: {}".format(finished_successfully)) 
---
+++ tests/methods.py
@@ -0,0 +1,18 @@
+
+
+### import home.matias.code.travistest.setup.py
+
+
+###import tests.test_setup
+
+
+
+def test_setup():
+
+    ### after importing this module, and if completed successfully,  
+    ### finished_successfully should be True
+    print('importing setup...')
+    import setup
+    assert setup.finished_successfully == True
+
+

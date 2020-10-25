0d3d0c4fbc1cb679032a432012da163cfca8c271
Matias <matiasnferrero@gmail.com>
2020-10-25 18:44:10 +0100

    fix

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,15 +2,8 @@
 ### import home.matias.code.travistest.setup.py
 ###import tests.test_setup
 
-import sys
-import shutil
-# insert at 1, 0 is the script path (or '' in REPL)
-source_path_notebooks = "D:/code/da-notebooks/notebooks/_modules"
-dest_path_notebooks = "D:/code/travis2/notebooks_to_test"
-shutil.copytree(source_path_notebooks, dest_path_notebooks)
 
 path_notebooks = 'notebooks_to_test'
-
 
 def test_notebooks_compatible_with_python_2():
     import os 
